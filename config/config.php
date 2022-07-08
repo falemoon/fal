1bd3b013c17aac70e828f0ce283ff1cdf631c689
falemoon <108959138+falemoon@users.noreply.github.com>
2022-07-09 02:46:26 +0430

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+class config{
+	public $bot_id             = 'TOKEN'; 
+	public $database_host      = 'localhost'; 
+	public $database_name      = 'database_name'; 
+	public $database_username  = 'database_username';
+	public $database_password  = 'e]database_password';
+	public $bot_Username       = 'ifaalgirbot';
+	public $path               = 'https://example.ir/falBot';
+	public $admin_list         = array(123,456,789);//admin list
+	public $version            = "3.2";
+	public $NameBOT            = 'ربات فالگیر فایل پیک';
+	public $CHANNEL_NAME       = 'فایل پیک';
+	public $CHANNEL_ID         = '@filepick';
+	public $CHANNEL_LINK       = 'http://t.me/filepick';
+	
+} 
